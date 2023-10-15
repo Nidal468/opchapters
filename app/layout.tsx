61b1860c1b66d746da50dc5114578bba72ce464c
Fate <115021956+Nidal468@users.noreply.github.com>
2023-10-15 14:14:17 +0600

    removed adds

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({children,}: {children: React.ReactNode}){
           <meta name='description' content='Hey everybody we are glad that you enjoy our scanlations but, dont forget to support the official version of our chapters or any other official publishing site! We value your suggestions so feel free to join our Discord server and give us feedback! for now just sit back, relax and enjoy some quality manga' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <link rel='icon' href='./favicon.ico' />
-          <Script data-cfasync="false" async type="text/javascript"src="https://iu.ecusemis.com/ry0O792CsgvVK9/nEOlO"/>
-          <Script strategy="afterInteractive" src="https://cdn.pubfuture-ad.com/v2/unit/pt.js"/>
+          {/**<Script data-cfasync="false" async type="text/javascript"src="https://iu.ecusemis.com/ry0O792CsgvVK9/nEOlO"/>
+          <Script strategy="afterInteractive" src="https://cdn.pubfuture-ad.com/v2/unit/pt.js"/>**/}
         </head>
         <body className={inter.className}>
           <Nav/>
